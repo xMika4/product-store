c3edba1f9e1ed60805f4a2bb5589a0e34ac5b91e
Mihael Baivić <xmika4@Mac.lan>
2024-12-25 20:41:42 +0100

    test

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+import {connectDB} from "./config/db.js";
+dotenv.config();
+const app = express();
+app.get("/products", (req, res) => {
+    res.send("Server is ready");
+})
+app.listen(3000, () => {
+    connectDB();
+    console.log("Server started at http://localhost:3000");
+})
