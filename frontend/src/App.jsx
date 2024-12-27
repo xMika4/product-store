f2a52915714e6cd30aa886e71651b49faa3e8d3b
Mihael Baivić <xmika4@Mac.lan>
2024-12-27 16:06:48 +0100

    frontend1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
-import { Button } from "@/components/ui/button"
+import { Box } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import { CreatePage } from "./components/pages/CreatePage";
+import { HomePage } from "./components/pages/Homepage";
+import { Navbar } from "./components/Navbar";
 function App() {
   return (
-    <>
-      <Button>Click me</Button>
-    </>
-  )
+    <Box minH={"100vh"}>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/create' element={<CreatePage />} />
+      </Routes>
+    </Box>
+  );
 }
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,15 @@
-import { Provider } from "@/components/ui/provider"
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { Provider } from "@/components/ui/provider";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Provider>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </StrictMode>
+);
